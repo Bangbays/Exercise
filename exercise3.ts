a43e84af8b2c37b9234d3a9533786155ff39fa95
Bangbays <amirullahakbar@gmail.com>
2025-01-30 18:39:17 +0700

    exercise3

---
+++ exercise3.ts
@@ -0,0 +1,28 @@
+// 1. Write a code to display the multiplication table of a given integer.
+
+const n1 = 9;
+for (let i = 1;i <= 10; i++){
+    console.log(n1 + "*" + i + "=" + n1 * i)
+}
+
+// 2. Write a code to check whether a string is a palindrome or not.
+
+const text = "madam";
+
+// 3. 
+
+const n2 = 200000;
+const km = n2 / 100000;
+
+console.log(km);
+
+// 4. 
+
+const n3 = 1000;
+const rupiah = n3.toLocaleString
+const format =  `Rp.${n3},00`;
+
+console.log(format)
+
+// 5. 
+
